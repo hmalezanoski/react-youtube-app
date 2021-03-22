84256804bbeed46f54a84c36966f6fa0d451de17
Hrisitjan Malezanoski <hmalezanoski@gmail.com>
2021-03-22 13:35:06 +0800

    init

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import react from 'react'
+import SearchBar from './searchBar'
+
+class App extends react.Component {
+
+
+    render() {
+        return <div><SearchBar/></div>
+    }
+
+}
+
+export default App
