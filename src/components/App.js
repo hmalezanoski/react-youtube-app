b474bc7d62ebec155a9a0cb0368bac74eaef161a
Hrisitjan Malezanoski <hmalezanoski@gmail.com>
2021-03-25 14:16:12 +0800

    styling video and vedeo details

--- src/components/App.js
+++ src/components/App.js
@@ -33,8 +33,16 @@ class App extends react.Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList onVideoSelect={this.onVideoSelect} videoList={this.state.videos} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videoList={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
